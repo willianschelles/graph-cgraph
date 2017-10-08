a4f9aad2f52ad35c741bd83a07d99ac860c5a5f1
willianssabino <willianschelles@gmail.com>
2017-10-08 11:19:47 -0300

    Adding a header file with:  All function to be implemented;  All data structure to be used;  All dependencies to develop.

---
+++ grafo.h
@@ -0,0 +1,171 @@
+#ifndef _GRAFO_H
+#define _GRAFO_H
+
+#include <stdio.h>
+
+typedef enum { false, true } bool;
+
+
+//------------------------------------------------------------------------------
+// o valor que representa "infinito"
+
+const int infinito;
+
+//------------------------------------------------------------------------------
+// (apontador para) estrutura de dados para representar um grafo
+// 
+// o grafo pode ser direcionado ou não
+// 
+// o grafo tem um nome, que é uma "string"
+
+typedef struct grafo *grafo;
+
+//------------------------------------------------------------------------------
+// devolve o nome do grafo g
+
+char *nome_grafo(grafo g);
+
+//------------------------------------------------------------------------------
+// devolve um grafo de nome s,
+//         ou 
+//         NULL, caso não exista tal grafo
+
+grafo grafo_nome(char *s);
+
+//------------------------------------------------------------------------------
+// devolve 1, se g é direcionado,
+//         ou 
+//         0, caso contrário
+
+int direcionado(grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o número de vértices do grafo g
+
+unsigned int numero_vertices(grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o número de arestas/arcos do grafo g
+
+unsigned int numero_arestas(grafo g);
+
+//------------------------------------------------------------------------------
+// desaloca toda a memória usada em *g
+// 
+// devolve 1 em caso de sucesso,
+//         ou 
+//         0, caso contrário
+
+int destroi_grafo(grafo g);
+
+//------------------------------------------------------------------------------
+// lê um grafo no formato dot de input
+// 
+// devolve o grafo lido,
+//         ou 
+//         NULL, em caso de erro 
+
+grafo le_grafo(FILE *input);  
+
+//------------------------------------------------------------------------------
+// escreve o grafo g em output usando o formato dot.
+//
+// devolve o grafo escrito,
+//         ou 
+//         NULL, em caso de erro 
+
+grafo escreve_grafo(FILE *output, grafo g);
+
+//------------------------------------------------------------------------------
+// (apontador para) estrutura de dados que representa um vértice do grafo
+// 
+// cada vértice tem um nome que é uma "string"
+
+typedef struct vertice *vertice;
+
+//------------------------------------------------------------------------------
+// devolve o nome do vertice v
+
+char *nome_vertice(vertice v);
+
+//------------------------------------------------------------------------------
+// devolve um vertice de nome s no grafo g,
+//         ou 
+//         NULL caso não exista em g um vertice de nome s
+
+vertice vertice_nome(char *s, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o grau do vértice v no grafo g
+// 
+// se g é direcionado, e 
+//                       direcao =  1, o grau devolvido é o de saída
+//                       direcao = -1, o grai devolvido é o de entrada
+//                
+// caso contrário o valor de direcao é ignorado.                  
+
+unsigned int grau(vertice v, int direcao, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o "primeiro" vizinho de v em g,
+//         ou
+//         NULL se v é vértice isolado em g
+//
+// se g é direcionado, e 
+//                       direcao =  1, o vizinho devolvido é de saída
+//                       direcao = -1, o vizinho devolvido é de entrada
+//                
+// caso contrário o valor de direcao é ignorado.                  
+
+vertice primeiro_vizinho(vertice v, int direcao, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o "próximo" vizinho de v em g após u,
+//         ou
+//         NULL se u é o "último" vizinho de v em g
+//
+// se g é direcionado, e 
+//                       direcao =  1, o vizinho devolvido é de saída
+//                       direcao = -1, o vizinho devolvido é de entrada
+//                
+// caso contrário o valor de direcao é ignorado.                  
+
+vertice proximo_vizinho(vertice u, vertice v, int direcao, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve 1, se g é um grafo bipartido, 
+//         ou
+//         0, caso contrário
+
+int bipartido( grafo g);
+
+//------------------------------------------------------------------------------
+// devolve em c um caminho mínimo de u a v no grafo não direcionado g, 
+//              de forma que
+//
+//                  c[0]=u, ..., c[n]=v, onde n é o tamanho do caminho
+//
+//              ou
+// 
+//              NULL se não existe tal caminho, 
+//
+// em qualquer caso, devolve a distância de u a v em g
+
+int caminho_minimo(vertice *c, vertice u, vertice v, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve 1, se v é um vértice simplicial em g, 
+//         ou
+//         0, caso contrário
+//
+// um vértice é simplicial no grafo se sua vizinhança é uma clique
+
+int simplicial(vertice v, grafo g);
+
+//------------------------------------------------------------------------------
+// devolve o diâmetro do grafo g
+
+int diametro(grafo g);
+
+//------------------------------------------------------------------------------
+#endif
